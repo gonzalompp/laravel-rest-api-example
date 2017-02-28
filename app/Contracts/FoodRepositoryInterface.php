c18774a9f28dbde9a86d216ff9cc0fbfc8500a33
Gonzalo Perez P <gonzalompp@gmail.com>
2017-02-27 23:37:24 -0300

    Interface updated with all the fields

--- app/Contracts/FoodRepositoryInterface.php
+++ app/Contracts/FoodRepositoryInterface.php
@@ -2,8 +2,18 @@
 
 namespace App\Contracts;
 
+use \stdClass;
+
 interface FoodRepositoryInterface {
     public function index();
+
+    public function store(stdClass $obj);
+
+    public function destroy($id);
+
+    public function update($id, stdClass $obj);
+
+    public function show($id);
 }
 
 ?>
